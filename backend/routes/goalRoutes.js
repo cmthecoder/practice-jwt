0933fcc8816c7c535c0dc1e0a381d0d5a9c1816c
Chris Mems <christopher.mems@.com>
2023-06-01 14:37:18 -0500

    create protect routes and controller functions for getting and creating a goal

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -6,10 +6,11 @@ const {
   udpateGoal,
   deleteGoal,
 } = require("../controllers/goalController");
+const {protect} = require('../middleware/authMiddleware')
 
-router.route('/').get(getGoals).post(setGoal)
+router.route('/').get(protect, getGoals).post(protect, setGoal)
 
-router.route('/:id').put(udpateGoal).delete(deleteGoal)
+router.route('/:id').put(protect, udpateGoal).delete(protect, deleteGoal)
 
 // router.get("/", getGoals);
 
