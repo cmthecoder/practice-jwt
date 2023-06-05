76a00067a534e0ad317dc64fd00dc02c4137e94a
Chris Mems <christopher.mems@.com>
2023-06-05 11:32:58 -0500

    get the goals to show in the ui

--- frontend/src/features/goals/goalSlice.js
+++ frontend/src/features/goals/goalSlice.js
@@ -30,6 +30,19 @@ export const goalSlice = createSlice({
       state.isError = true
       state.message = action.payload
     })
+    .addCase(getGoals.pending, (state) => {
+      state.isLoading = true
+    })
+    .addCase(getGoals.fulfilled, (state, action) => {
+      state.isLoading = false
+      state.isSuccess = true
+      state.goals = action.payload
+    })
+    .addCase(getGoals.rejected, (state, action) => {
+      state.isLoading = false
+      state.isError = true
+      state.message = action.payload
+    })
 
   }
 })
@@ -46,6 +59,18 @@ export const createGoal = createAsyncThunk('goals/create', async( goalData, thun
       return thunkAPI.rejectWithValue(message)
   }
 })
+
+// Get user goals
+export const getGoals = createAsyncThunk('goals/getAll', async (_, thunkAPI) => {
+  try {
+    const token = thunkAPI.getState().auth.user.token
+    return await goalService.getGoals(token)
+  } catch (error) {
+    const message = 
+    (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+    return thunkAPI.rejectWithValue(message)
+  }
+}) 
 
 export const {reset} = goalSlice.actions
 export default goalSlice.reducer
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,18 +1,39 @@
-import { useEffect } from "react"
-import {useNavigate} from 'react-router-dom'
-import {useSelector} from 'react-redux'
-import GoalForm from "../components/GoalForm"
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import GoalForm from "../components/GoalForm";
+import Spinner from "../components/Spinner";
+import { getGoals, reset } from "../features/goals/goalSlice";
+import GoalItem from "../components/GoalItem";
 
 const Dashboard = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-  const {user} = useSelector((state) => state.auth)
+  const { user } = useSelector((state) => state.auth);
+  const { goals, isLoading, isError, message } = useSelector(
+    (state) => state.goals
+  );
 
   useEffect(() => {
-    if(!user){
-      navigate('/login')
+    if (isError) {
+      console.log(message);
     }
-  }, [user, navigate])
+
+    if (!user) {
+      navigate("/login");
+    }
+
+    dispatch(getGoals());
+
+    return () => {
+      dispatch(reset());
+    };
+  }, [user, navigate, isError, message, dispatch]);
+
+  if (isLoading) {
+    return <Spinner />;
+  }
 
   return (
     <>
@@ -21,8 +42,19 @@ const Dashboard = () => {
         <p>Goals Dashboard</p>
       </section>
       <GoalForm />
+      <section className="content">
+        {goals.length > 0 ? (
+          <div className="goals">
+            {goals.map((goal) => (
+              <GoalItem key={goal._id} goal={goal} />
+            ))}
+          </div>
+        ) : (
+          <h3>You have not set any goals</h3>
+        )}
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
