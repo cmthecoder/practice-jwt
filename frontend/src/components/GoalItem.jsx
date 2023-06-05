3a93c68e875b349d47f82204e57b07562aa7fa45
Chris Mems <christopher.mems@.com>
2023-06-05 11:47:07 -0500

    create the functionality to delete a goal from the database

--- frontend/src/components/GoalItem.jsx
+++ frontend/src/components/GoalItem.jsx
@@ -1,13 +1,17 @@
+import { useDispatch } from "react-redux"
+import {deleteGoal} from '../features/goals/goalSlice'
 import { BsFillTrashFill } from "react-icons/bs"
 
 const GoalItem = ({goal}) => {
+  const dispatch = useDispatch()
+
   return (
     <div className='goal'>
       <div>
         {new Date(goal.createdAt).toLocaleString('en-US')}
       </div>
       <h2>{goal.text}</h2>
-      <button className="close"><BsFillTrashFill /></button>
+      <button onClick={() => dispatch(deleteGoal(goal._id))} className="close"><BsFillTrashFill /></button>
     </div>
   )
 }
